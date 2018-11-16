baa944539ca11048153be2c48a98b1cba9ce54a9
JihwanHwang <34638695+JihwanGo@users.noreply.github.com>
2018-11-16 16:38:02 +0900

    Update twitter.py

--- controller/twitter.py
+++ controller/twitter.py
@@ -4,9 +4,6 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     트위터와 OAUTH 연동하기 위한 모듈
-    
-    :copyright: (c) 2013-2016 by 4mba.
-    :license: MIT LICENSE 2.0, see license for more details.
 """
 
 
